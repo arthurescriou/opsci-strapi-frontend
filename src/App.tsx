7f5b5567ab7286c9d32b587c5db2b04f6dc0e690
arthur.escriou <arthur.escriou@gmail.com>
2024-03-19 14:16:02 +0100

    add event

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,31 @@ type Product = {
   barcode?: string
   updatedAt: string
   createdAt: string
+  status: 'safe' | 'danger' | 'empty'
+}
+
+type Event = {
+  updatedAt: string
+  createdAt: string
+  value: string
+  metadata: any
+}
+
+const getColor = (status?: 'safe' | 'danger' | 'empty') => {
+  switch (status) {
+    case 'safe':
+      return '#00b200'
+    case 'danger':
+      return '#ee6002'
+    case 'empty':
+      return '#ff2410'
+    default:
+      return 'grey'
+  }
 }
 
 const ProductCard = ({ value }: { value: Product }) => (
-  <div className="product-card">
+  <div className="product-card" style={{ background: getColor(value.status) }}>
     <div className="product-card-name">{value.name}</div>
     <div className="product-card-desc">{value.description}</div>
     <div className="product-card-stock">
@@ -32,22 +53,75 @@ const ProductCard = ({ value }: { value: Product }) => (
   </div>
 )
 
+const EventLine = ({ value }: { value: Event }) => (
+  <div
+    className="event-line"
+    title={value.value}
+    onClick={() => {
+      navigator.clipboard.writeText(value.value)
+    }}
+  >
+    {formatDate(value.createdAt) + ': '}
+    {value.value}
+  </div>
+)
+
+const fetchAllPages = async (
+  page = 1,
+  pagination = true,
+  sort = 'createdAt'
+) => {
+  const res = await fetch(
+    URL + `/api/products?sort=${sort}&pagination[page]=${page}`,
+    {
+      headers: { Authorization: `Bearer ${TOKEN}` },
+    }
+  ).then((r) => r.json())
+  let result = res.data
+  if (pagination && res.meta.pagination.page < res.meta.pagination.pageCount) {
+    const rest = await fetchAllPages(page + 1, pagination, sort)
+    result = [...result, ...rest]
+  }
+  return result
+}
+
+const fetchAllEvents = async (page = 1, pagination = true) => {
+  const res = await fetch(
+    URL + '/api/events?sort=createdAt:desc&pagination[page]=' + page,
+    {
+      headers: { Authorization: `Bearer ${TOKEN}` },
+    }
+  ).then((r) => r.json())
+  let result = res.data
+  if (pagination && res.meta.pagination.page < res.meta.pagination.pageCount) {
+    const rest = await fetchAllEvents(page + 1)
+    result = [...result, ...rest]
+  }
+  return result
+}
+
 const App = () => {
   const [values, setValues] = useState<{ attributes: Product; id: number }[]>(
     []
   )
+  const [events, setEvents] = useState<{ attributes: Event; id: number }[]>([])
+
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState<string>()
 
   useEffect(() => {
     const interval = setInterval(() => {
-      fetch(URL + '/api/products', {
-        headers: { Authorization: `Bearer ${TOKEN}` },
-      })
-        .then((r) => r.json())
+      fetchAllEvents()
+        .then((v) => {
+          console.log(v)
+          setEvents(v)
+        })
+        .catch(console.error)
+
+      fetchAllPages()
         .then((v) => {
           setLoading(false)
-          setValues(v.data)
+          setValues(v)
           setError(null)
         })
         .catch((err) => {
@@ -70,10 +144,19 @@ const App = () => {
           <div></div>
         </div>
       ) : (
-        <div className="card-container">
-          {values.map((value) => (
-            <ProductCard value={value.attributes} key={value.id} />
-          ))}
+        <div className="page-container">
+          {events.length > 0 && (
+            <div className="event-container">
+              {events.map((event) => (
+                <EventLine key={event.id} value={event.attributes} />
+              ))}
+            </div>
+          )}
+          <div className="card-container">
+            {values.map((value) => (
+              <ProductCard value={value.attributes} key={value.id} />
+            ))}
+          </div>
         </div>
       )}
     </div>
